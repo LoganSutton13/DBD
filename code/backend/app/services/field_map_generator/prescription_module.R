92be7a53e05d3d9cf9288fbc3de89d4b3229ec3c
Gil Rezin <gil.rezin@hotmail.com>
2026-02-13 12:44:06 -0800

    cleaned up & documented prescription_module

--- code/backend/app/services/field_map_generator/prescription_module.R
+++ code/backend/app/services/field_map_generator/prescription_module.R
@@ -11,53 +11,18 @@ library(stars)
 library(dbscan)
 library(imager)
 
+# Precondition: current working directory must be :/code/backend/app/services/field_map_generator
 source("fieldShapeModified.R")
 
 # TODO: write function to set ideal field resolution with parameter for minimum cell resolution (boom sprayer size)
 
-# RoundUp <- function(x, increment) {
-#   ceiling(x / increment) * increment
-# }
 
-# calculate_cluster_likelihood <- function(df, n_cols) {
-#   n <- nrow(df)
-#   n_rows <- ceiling(n / n_cols)
-#   
-#   # Create cluster matrix
-#   cluster_matrix <- matrix(NA, nrow = n_rows, ncol = n_cols)
-#   cluster_matrix[seq_len(n)] <- df$cluster
-#   
-#   # Initialize counts
-#   match_count <- integer(n)
-#   total_count <- integer(n)
-#   
-#   # Check all 4 directions using matrix operations
-#   # Below
-#   valid <- seq_len(n)[seq_len(n) > n_cols]
-#   match_count[valid] <- match_count[valid] + (cluster_matrix[valid] == cluster_matrix[valid - n_cols])
-#   total_count[valid] <- total_count[valid] + 1
-#   
-#   # Above
-#   valid <- seq_len(n)[seq_len(n) <= n - n_cols]
-#   match_count[valid] <- match_count[valid] + (cluster_matrix[valid] == cluster_matrix[valid + n_cols])
-#   total_count[valid] <- total_count[valid] + 1
-#   
-#   # Left
-#   valid <- seq_len(n)[((seq_len(n) - 1) %% n_cols) != 0]
-#   match_count[valid] <- match_count[valid] + (cluster_matrix[valid] == cluster_matrix[valid - 1])
-#   total_count[valid] <- total_count[valid] + 1
-#   
-#   # Right
-#   valid <- seq_len(n)[((seq_len(n) - 1) %% n_cols) != (n_cols - 1) & seq_len(n) < n]
-#   match_count[valid] <- match_count[valid] + (cluster_matrix[valid] == cluster_matrix[valid + 1])
-#   total_count[valid] <- total_count[valid] + 1
-#   
-#   # Calculate likelihood
-#   df$cluster_likelihood <- ifelse(total_count > 0, match_count / total_count, 0)
-#   
-#   return(df)
-# }
-
+# function: smoothen_field
+# smoothens the data gradient of NDVI information contained within a field consisting of cells
+# field: a data frame containing the column 'NDVI_max'
+# rows: the number of rows contained in the field.
+# sigma: the kernel size of the smoothing operation (n by n)
+# Precondition: the number of rows must be the same as the number of columns.
 smoothen_field <- function(field, rows, sigma = 1) {
   # Convert to matrix
   ndvi_matrix <- matrix(field$NDVI_max, nrow = rows, ncol = rows, byrow = FALSE)
@@ -75,6 +40,7 @@ smoothen_field <- function(field, rows, sigma = 1) {
   return(field)
 }
 
+# function: generate_prescription
 # Generates a prescription map from stitched orthophoto drone imagery.
 # orthophoto: stitched drone image filepath, file generated with WebODM
 # heading: in degrees, the heading that the robot will use on the field
@@ -111,6 +77,7 @@ generate_prescription <- function (orthophoto, heading = 0, cell_size = 4.571, c
     NDVI_cell_info$NDVI_max <- smoothen_field(NDVI_cell_info, field_grid$rows, sigma=smoothing_sigma)$NDVI_max
   }
   
+  # structure the dataset for clustering
   NDVI_cluster_data <- NDVI_cell_info %>%
     mutate(
       boundary = geometry,
@@ -134,7 +101,7 @@ generate_prescription <- function (orthophoto, heading = 0, cell_size = 4.571, c
   NDVI_cluster_data <- NDVI_cluster_data %>%
     rowwise() %>%
     
-    # TODO: fix this, clusters should be based on max, not min
+    # TODO: fix this, clusters should be based on max, not min to prevent clustering items below & above the NDVI threshold within the last cluster
     mutate(cluster = min(min(which(intervals >= NDVI_max)), cluster_count)) %>%
     ungroup()
   
@@ -142,82 +109,6 @@ generate_prescription <- function (orthophoto, heading = 0, cell_size = 4.571, c
        col = NDVI_cluster_data$cluster,
        pch = 20)
     
-  
-  # cluster_interval <- ((abs(min(NDVI_cell_info$NDVI_max)) + abs(max(NDVI_cell_info$NDVI_max))) / cluster_count)
-  # NDVI_cell_info <- NDVI_cell_info %>%
-  #   mutate(cluster = (NDVI_max + abs(min(NDVI_max))) %/% cluster_interval)
-  # NDVI_cell_info$cluster <- pmin(NDVI_cell_info$cluster, cluster_count-1)
-  
-  ############################################################################################################
-  
-  
-  # ############################################################################################################
-  # # clustering with consensus HDBSCAN
-  # 
-  # #NDVI_cell_info[paste0("cluster_prob_", 1:cluster_count)] <- NA
-  # 
-  # # cluster our dataset multiple times over, use a consensus model
-  # for (i in seq(from=0.2, to=0.3, by=0.01))
-  # {
-  #   NDVI_cluster_data <- NDVI_cell_info %>%
-  #     mutate(
-  #       boundary = geometry,
-  #       centerpoint = st_centroid(geometry),
-  #       easting = st_coordinates(centerpoint)[, 1],
-  #       northing = st_coordinates(centerpoint)[, 2],
-  #       NDVI_max = RoundUp(NDVI_max_smoothened, i)
-  #       
-  #     ) %>%
-  #     #mutate(NDVI_max = NDVI_max * 1) %>%  # 3-dimensional radius means we need to spread out our NDVI Values more
-  #     select(NDVI_max, easting, northing, PlotID) %>%
-  #     st_drop_geometry()
-  #   
-  #   # normalization (PREFERRED OPTION)
-  #   NDVI_cluster_data_normalized <- scale(NDVI_cluster_data)
-  #   
-  #   dbscan_result <- hdbscan(NDVI_cluster_data_normalized, minPts = 8)
-  #   
-  #   #TODO: get the averages for each cluster in dbscan_result
-  #   cluster_results <- NDVI_cluster_data
-  #   cluster_results$cluster <- dbscan_result$cluster
-  #   cluster_results$cluster_likelihood_total <- 0
-  #   
-  #   NDVI_cluster_likelihoods <- calculate_cluster_likelihood(NDVI_cluster_data, field_grid$rows)
-  #   
-  #   # cluster_averages <- lapply(unique(NDVI_cluster_data[NDVI_cluster_data$cluster > 0,]$cluster), function(i) {
-  #   #   mean(NDVI_cluster_data[NDVI_cluster_data$cluster == i, ]$NDVI_max)
-  #   # })
-  #   
-  #   
-  # }
-  # ############################################################################################################
-  # 
-  # for (i in seq(from=0.2, to=0.3, by=0.01))
-  # {
-  #   NDVI_cluster_data <- NDVI_cell_info %>%
-  #     mutate(
-  #       boundary = geometry,
-  #       centerpoint = st_centroid(geometry),
-  #       easting = st_coordinates(centerpoint)[, 1],
-  #       northing = st_coordinates(centerpoint)[, 2],
-  #       NDVI_max = RoundUp(NDVI_max_smoothened, i)
-  #     ) %>%
-  #     mutate(NDVI_max = NDVI_max * 1) %>%  # 3-dimensional radius means we need to spread out our NDVI Values more
-  #     select(NDVI_max, easting, northing) %>%
-  #     st_drop_geometry()
-  #   
-  #   # normalization (PREFERRED OPTION)
-  #   NDVI_cluster_data_normalized <- scale(NDVI_cluster_data)
-  #   
-  #   # run dbscan in 3-dimensions (latitude, longitude, NDVI)
-  #   dbscan_result <- hdbscan(NDVI_cluster_data_normalized, minPts = 8)
-  #   table(dbscan_result$cluster) # view the cluster results
-  #   plot(NDVI_cluster_data$easting, NDVI_cluster_data$northing,
-  #        col = dbscan_result$cluster+1,
-  #        pch = 20)
-  # }
-  
-  
   prescription_map <- NDVI_cluster_data %>%
     mutate(
       # convert UTM easting / northing into latitude / longitude
@@ -229,11 +120,8 @@ generate_prescription <- function (orthophoto, heading = 0, cell_size = 4.571, c
     ) %>%
     select(PlotID, NDVI_max, boundary_latlong, cluster, centerpoint_longitude, centerpoint_latitude)
   
-  # bring dbscan results into points dataset
-  #prescription_map$cluster <- dbscan_result$cluster
-  
-  
   # convert coordinate list to a SpatVector
+  # TODO: output data frame to a shapefile instead of a GeoJSON
   print("Finishing up...")
   prescription_map_vector <- vect(prescription_map, geom = c("centerpoint_longitude", "centerpoint_latitude"), crs="EPSG:4326")
   writeVector(prescription_map_vector, paste(outputFilePath, outputFileName), filetype = "GeoJSON", overwrite = TRUE)
@@ -256,9 +144,3 @@ smoothing_sigma = 10
 cell_size = 4.5
 cluster_count = 3
 ##
-
-# NDVI_Cells <- generate_prescription("../data/odm_orthophoto.tif", heading = 45)
-# 
-# scatterplot3d(x = NDVI_Cells$easting, y = NDVI_Cells$northing, z = NDVI_Cells$NDVI_max,
-#               xlab = "easting (m)", ylab = "northing (m)", zlab = "NDVI max",
-#               main = "easting vs. northing vs. NDVI max", pch = 16, color = "red")
